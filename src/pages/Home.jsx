443f79858c9e1620ed57000e7479f18f9838e295
Niraj-Kr-29 <nkr2348@gmail.com>
2024-11-09 00:44:37 +0530

    Home Page image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CategoryCard from '../components/CategoryCard';
 import { BackgroundBeamsWithCollision } from '../components/ui/background-beams-with-collision';
 import { useSelector } from 'react-redux';
+import HeroImage from '../assets/HeroImage.svg'
 
 function Home() {
     const authStatus = useSelector((state)=>
@@ -34,8 +35,10 @@ function Home() {
   return (
     <BackgroundBeamsWithCollision>
         <div className='flex items-center flex-col justify-center px-2 md:px-10 pt-40  py-4 w-full h-screen overflow-scroll scroll-smooth no-scrollbar'>
-
-            <div className='m-6 sm:text-xl md:text-4xl text-lg mb-16 text-center mt-[25rem] sm:mt-0'>DIVE DEEP INTO THE OCEAN OF KNOWLEDGE ON <span className='bg-gradient-to-r from-customOrange to-yellow-500 bg-clip-text text-transparent'>VARIOUS DOMAINS</span></div>
+            <div className='flex w-full h-full'>
+            <div className='m-6 sm:text-xl md:text-4xl text-lg mb-16 text-center mt-[25rem] sm:mt-0 w-[70%] content-center'>DIVE DEEP INTO THE OCEAN OF KNOWLEDGE ON <span className='bg-gradient-to-r from-customOrange to-yellow-500 bg-clip-text text-transparent'>VARIOUS DOMAINS</span></div>
+            <div className='w-[30%] md:pt-0 pt-80'><img src={HeroImage} alt="" className='w-full h-full' /></div>
+            </div>
             <div className='flex justify-center'>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between gap-10 md:gap-16">
                     {
